0c2964d8bda3fd3124a194b37fc3e1fede4f2353
elnagar291-pixel <elnagar291@gmail.com>
2026-09-22 16:17:08 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'khair-altabia-v2.0.0';
+const CORE = ['./','./index.html','./manifest.json','./sw.js','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('message', event => { if(event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting(); });
+self.addEventListener('fetch', event => {
+  if(event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if(url.origin !== location.origin) return;
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(c => c.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
+  );
+});
