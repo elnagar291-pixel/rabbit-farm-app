3222aa417902fb68692328f8c540bb5bb6c99bc2
elnagar291-pixel <elnagar291@gmail.com>
2026-09-23 12:44:10 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'khair-tabeaa-v2.4.0';
+const CACHE_NAME = 'khair-tabeaa-v2.5.0';
 const CORE_ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
